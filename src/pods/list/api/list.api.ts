f29157051067b92eeda20596988cd5d0dd577654
devopsturing <devopsturing@cpifpalanturing.es>
2026-05-04 19:47:08 +0200

    upload files

--- src/pods/list/api/list.api.ts
+++
@@ -1,6 +0,0 @@
-import { Member } from './list.api-model';
-
-export const getMemberList = async (organization: string): Promise<Member[]> =>
-  await fetch(`https://api.github.com/orgs/${organization}/members`).then(
-    (response) => response.json()
-  );
